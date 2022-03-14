7031b019b90b0b5e429b625d7fe3c469f250c1f5
madalinamart <91364600+madalinamart@users.noreply.github.com>
2022-03-14 11:03:13 +0200

    added login functionalities

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+export default axios.create({
+    baseURL: 'http://localhost:3006'
+});
+
 const url = 'http://localhost:3006/employees'
 
 export const getUsers = async (id) => {
--- src/components/Login/useForm.js
+++ src/components/Login/useForm.js
@@ -1,12 +1,20 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useContext} from 'react'
+import AuthContext from '../../context/AuthProvider'
+import axios from 'axios'
+
+const LOGIN_URL = '/auth'
+
 
 //LOGIN FEATURE
 
 const useForm = (callback, validateLogin) => {
-    const [values, setValues] = useState({
+    const {setAuth} = useContext(AuthContext);
+     const [values, setValues] = useState({
         email: '',
         password: ''
-    })  
+    })   
+
+    
 
     const [errors, setErrors] = useState({})
     const [isSubmitting, setIsSubmitting] = useState(false)
@@ -19,11 +27,32 @@ const useForm = (callback, validateLogin) => {
         })
     }
 
-    const handleSubmit = e => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
-        setErrors(validateLogin(values));
-        setIsSubmitting(true)
+        try {
+            const response = await axios.post(LOGIN_URL, 
+                JSON.stringify({email: values.email, password: values.password}),
+                {headers: { 'Content-Type': 'application/json'},
+                withCredentials: true
+            })
+           const credentials = JSON.stringify(response?.data)
+           console.log(credentials)            
+            setAuth({ email:values.email, password: values.password})
+            setErrors(validateLogin(values));
+            setIsSubmitting(true)
+        } catch (error) {
+            if(!error?.response) {
+                console.log('No server Response')
+            } else if (error.response?.status === 400) {
+                console.log('Missing Username or Password')
+            } else if(error.response?.status === 401) {
+                console.log('Unauthorized')
+            } else {
+                console.log('Login Failed')
+            }
+        }
+
     }
 
     useEffect(() => {
